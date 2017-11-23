5e342c24ab67357df39545ee3b215ccf286054e1
Liz Burton <liz.burton147@gmail.com>
2017-11-23 22:54:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@ require 'pry'
 # Add  code here!
 def prime?(integer)
   if integer > 0
-    array = (2..integer).to_a
+    array = (2..integer-1).to_a
   elsif integer < 0
-    array = (integer..-2).to_a
+    array = (integer-1..-2).to_a
   end
   array.each do | divisor |
     if integer % divisor == 0
