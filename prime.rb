40f88ddccabebe96af2fa1bd8c3a11e04c2d6683
Liz Burton <liz.burton147@gmail.com>
2017-11-23 23:03:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,15 @@
 require 'pry'
 # Add  code here!
 def prime?(integer)
-  if integer > 0
-    array = (2..integer-1).to_a
-  elsif integer < 0
-    array = (integer+1..-2).to_a
-  end
-  binding.pry
-  array.each do | divisor |
-    if integer % divisor == 0
-      return false
+  if integer.abs < 2
+    return false
+  else
+    array = (2..integer-1.abs).to_a
+    array.each do | divisor |
+      if integer % divisor == 0
+        return false
+      end
     end
   end
   true
 end
-binding.pry
